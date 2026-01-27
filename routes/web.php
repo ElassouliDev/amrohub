22f0c22eb0a6a8e29010712a909bf7287692cb9b
Yehia Elassouli <elassoulidev@gmail.com>
2026-01-27 20:00:59 +0200

    add filment  package

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,15 @@ Route::post('join-requests', [JoinRequestController::class,'store'])->name('join
 
 
 
-Route::prefix('dashboard')->group(function () {
-    Route::resource('join-requests', \App\Http\Controllers\Dashboard\JoinRequestController::class)->only('index', 'show' ,'edit','update','destroy');
-    Route::put('join-requests/{joinRequest}/update-status', [\App\Http\Controllers\Dashboard\JoinRequestController::class,'updateStatus'])->name('join-requests.update-status' );
-    Route::resource('plans', PlanController::class);
+// Route::prefix('dashboard')->group(function () {
+//     Route::resource('join-requests', \App\Http\Controllers\Dashboard\JoinRequestController::class)->only('index', 'show' ,'edit','update','destroy');
+//     Route::put('join-requests/{joinRequest}/update-status', [\App\Http\Controllers\Dashboard\JoinRequestController::class,'updateStatus'])->name('join-requests.update-status' );
+//     Route::resource('plans', PlanController::class);
 
-    Route::resource('user-plans', UserPlanController::class);
-    Route::resource('usage-logs', UsageLogController::class);
-    Route::post('usage-logs/start', [UsageLogController::class, 'startSession'])->name('usage-logs.start');
-    Route::post('usage-logs/end', [UsageLogController::class, 'endSession'])->name('usage-logs.end');
+//     Route::resource('user-plans', UserPlanController::class);
+//     Route::resource('usage-logs', UsageLogController::class);
+//     Route::post('usage-logs/start', [UsageLogController::class, 'startSession'])->name('usage-logs.start');
+//     Route::post('usage-logs/end', [UsageLogController::class, 'endSession'])->name('usage-logs.end');
 
-});
+// });
 
